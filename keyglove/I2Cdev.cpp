780733724b57b64d4a515a035564667151d8938c
Jeff Rowberg <jeff@rowberg.net>
2011-09-26 12:09:52 -0400

    Update new files, convert to Arduino 1.0 compatibility

---
+++ keyglove/I2Cdev.cpp
@@ -0,0 +1,485 @@
+// I2Cdev library collection - Main I2C device class
+// Abstracts bit and byte I2C R/W functions into a convenient class
+// 8/31/2011 by Jeff Rowberg <jeff@rowberg.net>
+//
+// Changelog:
+//     2011-08-31 - added support for Arduino 1.0 Wire library (methods are different from 0.x)
+//     2011-08-03 - added optional timeout parameter to read* methods to easily change from default
+//     2011-08-02 - added support for 16-bit registers
+//                - fixed incorrect Doxygen comments on some methods
+//                - added timeout value for read operations (thanks mem @ Arduino forums)
+//     2011-07-30 - changed read/write function structures to return success or byte counts
+//                - made all methods static for multi-device memory savings
+//     2011-07-28 - initial release
+
+/* ============================================
+I2Cdev device library code is placed under the MIT license
+Copyright (c) 2011 Jeff Rowberg
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+===============================================
+*/
+
+#include "I2Cdev.h"
+
+/** Default constructor.
+ */
+I2Cdev::I2Cdev() {
+}
+
+/** Read a single bit from an 8-bit device register.
+ * @param devAddr I2C slave device address
+ * @param regAddr Register regAddr to read from
+ * @param bitNum Bit position to read (0-7)
+ * @param data Container for single bit value
+ * @param timeout Optional read timeout in milliseconds (0 to disable, leave off to use default class value in I2Cdev::readTimeout)
+ * @return Status of read operation (true = success)
+ */
+int8_t I2Cdev::readBit(uint8_t devAddr, uint8_t regAddr, uint8_t bitNum, uint8_t *data, uint16_t timeout) {
+    uint8_t b;
+    uint8_t count = readByte(devAddr, regAddr, &b, timeout);
+    *data = b & (1 << bitNum);
+    return count;
+}
+
+/** Read a single bit from a 16-bit device register.
+ * @param devAddr I2C slave device address
+ * @param regAddr Register regAddr to read from
+ * @param bitNum Bit position to read (0-15)
+ * @param data Container for single bit value
+ * @param timeout Optional read timeout in milliseconds (0 to disable, leave off to use default class value in I2Cdev::readTimeout)
+ * @return Status of read operation (true = success)
+ */
+int8_t I2Cdev::readBitW(uint8_t devAddr, uint8_t regAddr, uint8_t bitNum, uint16_t *data, uint16_t timeout) {
+    uint16_t b;
+    uint8_t count = readWord(devAddr, regAddr, &b, timeout);
+    *data = b & (1 << bitNum);
+    return count;
+}
+
+/** Read multiple bits from an 8-bit device register.
+ * @param devAddr I2C slave device address
+ * @param regAddr Register regAddr to read from
+ * @param bitStart First bit position to read (0-7)
+ * @param length Number of bits to read (not more than 8)
+ * @param data Container for right-aligned value (i.e. '101' read from any bitStart position will equal 0x05)
+ * @param timeout Optional read timeout in milliseconds (0 to disable, leave off to use default class value in I2Cdev::readTimeout)
+ * @return Status of read operation (true = success)
+ */
+int8_t I2Cdev::readBits(uint8_t devAddr, uint8_t regAddr, uint8_t bitStart, uint8_t length, uint8_t *data, uint16_t timeout) {
+    // 01101001 read byte
+    // 76543210 bit numbers
+    //    xxx   args: bitStart=4, length=3
+    //    010   masked
+    //   -> 010 shifted
+    uint8_t count, b, r = 0;
+    if ((count = readByte(devAddr, regAddr, &b, timeout)) != 0) {
+        for (uint8_t i = bitStart; i > bitStart - length; i--) {
+            r |= (b & (1 << i));
+        }
+        r >>= (bitStart - length + 1);
+        *data = r;
+    }
+    return count;
+}
+
+/** Read multiple bits from a 16-bit device register.
+ * @param devAddr I2C slave device address
+ * @param regAddr Register regAddr to read from
+ * @param bitStart First bit position to read (0-15)
+ * @param length Number of bits to read (not more than 16)
+ * @param data Container for right-aligned value (i.e. '101' read from any bitStart position will equal 0x05)
+ * @param timeout Optional read timeout in milliseconds (0 to disable, leave off to use default class value in I2Cdev::readTimeout)
+ * @return Status of read operation (1 = success, 0 = failure, -1 = timeout)
+ */
+int8_t I2Cdev::readBitsW(uint8_t devAddr, uint8_t regAddr, uint8_t bitStart, uint8_t length, uint16_t *data, uint16_t timeout) {
+    // 1101011001101001 read byte
+    // fedcba9876543210 bit numbers
+    //    xxx           args: bitStart=12, length=3
+    //    010           masked
+    //           -> 010 shifted
+    uint8_t count;
+    uint16_t w, r = 0;
+    if ((count = readWord(devAddr, regAddr, &w, timeout)) != 0) {
+        for (uint8_t i = bitStart; i > bitStart - length; i--) {
+            r |= (w & (1 << i));
+        }
+        r >>= (bitStart - length + 1);
+        *data = r;
+    }
+    return count;
+}
+
+/** Read single byte from an 8-bit device register.
+ * @param devAddr I2C slave device address
+ * @param regAddr Register regAddr to read from
+ * @param data Container for byte value read from device
+ * @param timeout Optional read timeout in milliseconds (0 to disable, leave off to use default class value in I2Cdev::readTimeout)
+ * @return Status of read operation (true = success)
+ */
+int8_t I2Cdev::readByte(uint8_t devAddr, uint8_t regAddr, uint8_t *data, uint16_t timeout) {
+    return readBytes(devAddr, regAddr, 1, data, timeout);
+}
+
+/** Read single word from a 16-bit device register.
+ * @param devAddr I2C slave device address
+ * @param regAddr Register regAddr to read from
+ * @param data Container for word value read from device
+ * @param timeout Optional read timeout in milliseconds (0 to disable, leave off to use default class value in I2Cdev::readTimeout)
+ * @return Status of read operation (true = success)
+ */
+int8_t I2Cdev::readWord(uint8_t devAddr, uint8_t regAddr, uint16_t *data, uint16_t timeout) {
+    return readWords(devAddr, regAddr, 1, data, timeout);
+}
+
+/** Read multiple bytes from an 8-bit device register.
+ * @param devAddr I2C slave device address
+ * @param regAddr First register regAddr to read from
+ * @param length Number of bytes to read
+ * @param data Buffer to store read data in
+ * @param timeout Optional read timeout in milliseconds (0 to disable, leave off to use default class value in I2Cdev::readTimeout)
+ * @return Number of bytes read (0 indicates failure)
+ */
+int8_t I2Cdev::readBytes(uint8_t devAddr, uint8_t regAddr, uint8_t length, uint8_t *data, uint16_t timeout) {
+    #ifdef I2CDEV_SERIAL_DEBUG
+        Serial.print("I2C (0x");
+        Serial.print(devAddr, HEX);
+        Serial.print(") reading ");
+        Serial.print(length, DEC);
+        Serial.print(" bytes from 0x");
+        Serial.print(regAddr, HEX);
+        Serial.print("...");
+    #endif
+
+    int8_t count = 0;
+
+    Wire.beginTransmission(devAddr);
+#if (I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO0_WIRE)
+    Wire.send(regAddr);
+#elif (I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO1_WIRE)
+    Wire.write(&regAddr, 1);
+#endif
+    Wire.endTransmission();
+
+    Wire.beginTransmission(devAddr);
+    Wire.requestFrom(devAddr, length);
+
+    uint32_t t1 = millis();
+    for (; Wire.available() && (timeout == 0 || millis() - t1 < timeout); count++) {
+#if (I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO0_WIRE)
+        data[count] = Wire.receive();
+#elif (I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO1_WIRE)
+        data[count] = Wire.read();
+#endif
+        #ifdef I2CDEV_SERIAL_DEBUG
+            Serial.print(data[count], HEX);
+            Serial.print(" ");
+        #endif
+    }
+    if (timeout > 0 && millis() - t1 >= timeout && count < length) count = -1; // timeout
+
+    Wire.endTransmission();
+
+    #ifdef I2CDEV_SERIAL_DEBUG
+        Serial.print(". Done (");
+        Serial.print(count, DEC);
+        Serial.println(" read).");
+    #endif
+
+    return count;
+}
+
+/** Read multiple words from a 16-bit device register.
+ * @param devAddr I2C slave device address
+ * @param regAddr First register regAddr to read from
+ * @param length Number of words to read
+ * @param data Buffer to store read data in
+ * @param timeout Optional read timeout in milliseconds (0 to disable, leave off to use default class value in I2Cdev::readTimeout)
+ * @return Number of words read (0 indicates failure)
+ */
+int8_t I2Cdev::readWords(uint8_t devAddr, uint8_t regAddr, uint8_t length, uint16_t *data, uint16_t timeout) {
+    #ifdef I2CDEV_SERIAL_DEBUG
+        Serial.print("I2C (0x");
+        Serial.print(devAddr, HEX);
+        Serial.print(") reading ");
+        Serial.print(length, DEC);
+        Serial.print(" words from 0x");
+        Serial.print(regAddr, HEX);
+        Serial.print("...");
+    #endif
+
+    int8_t count = 0;
+
+    Wire.beginTransmission(devAddr);
+#if (I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO0_WIRE)
+    Wire.send(regAddr);
+#elif (I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO1_WIRE)
+    Wire.write(&regAddr, 1);
+#endif
+    Wire.endTransmission();
+
+    Wire.beginTransmission(devAddr);
+    Wire.requestFrom(devAddr, (uint8_t)(length * 2)); // length=words, this wants bytes
+
+    uint32_t t1 = millis();
+    bool msb = true; // starts with MSB, then LSB
+    for (; Wire.available() && count < length && (timeout == 0 || millis() - t1 < timeout);) {
+        if (msb) {
+            // first byte is bits 15-8 (MSb=15)
+#if (I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO0_WIRE)
+            data[count] = Wire.receive() << 8;
+#elif (I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO1_WIRE)
+            data[count] = Wire.read() << 8;
+#endif
+        } else {
+            // second byte is bits 7-0 (LSb=0)
+#if (I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO0_WIRE)
+            data[count] |= Wire.receive();
+#elif (I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO1_WIRE)
+            data[count] |= Wire.read();
+#endif
+            #ifdef I2CDEV_SERIAL_DEBUG
+                Serial.print(data[count], HEX);
+                Serial.print(" ");
+            #endif
+            count++;
+        }
+        msb = !msb;
+    }
+    if (timeout > 0 && millis() - t1 >= timeout && count < length) count = -1; // timeout
+
+    Wire.endTransmission();
+
+    #ifdef I2CDEV_SERIAL_DEBUG
+        Serial.print(". Done (");
+        Serial.print(count, DEC);
+        Serial.println(" read).");
+    #endif
+    
+    return count;
+}
+
+/** write a single bit in an 8-bit device register.
+ * @param devAddr I2C slave device address
+ * @param regAddr Register regAddr to write to
+ * @param bitNum Bit position to write (0-7)
+ * @param value New bit value to write
+ * @return Status of operation (true = success)
+ */
+bool I2Cdev::writeBit(uint8_t devAddr, uint8_t regAddr, uint8_t bitNum, uint8_t data) {
+    uint8_t b;
+    readByte(devAddr, regAddr, &b);
+    b = (data != 0) ? (b | (1 << bitNum)) : (b & ~(1 << bitNum));
+    return writeByte(devAddr, regAddr, b);
+}
+
+/** write a single bit in a 16-bit device register.
+ * @param devAddr I2C slave device address
+ * @param regAddr Register regAddr to write to
+ * @param bitNum Bit position to write (0-15)
+ * @param value New bit value to write
+ * @return Status of operation (true = success)
+ */
+bool I2Cdev::writeBitW(uint8_t devAddr, uint8_t regAddr, uint8_t bitNum, uint16_t data) {
+    uint16_t w;
+    readWord(devAddr, regAddr, &w);
+    w = (data != 0) ? (w | (1 << bitNum)) : (w & ~(1 << bitNum));
+    return writeWord(devAddr, regAddr, w);
+}
+
+/** Write multiple bits in an 8-bit device register.
+ * @param devAddr I2C slave device address
+ * @param regAddr Register regAddr to write to
+ * @param bitStart First bit position to write (0-7)
+ * @param length Number of bits to write (not more than 8)
+ * @param data Right-aligned value to write
+ * @return Status of operation (true = success)
+ */
+bool I2Cdev::writeBits(uint8_t devAddr, uint8_t regAddr, uint8_t bitStart, uint8_t length, uint8_t data) {
+    //      010 value to write
+    // 76543210 bit numbers
+    //    xxx   args: bitStart=4, length=3
+    // 01000000 shift left (8 - length)    ]
+    // 00001000 shift right (7 - bitStart) ] --- two shifts ensure all non-important bits are 0
+    // 11100011 mask byte
+    // 10101111 original value (sample)
+    // 10100011 original & mask
+    // 10101011 masked | value
+    uint8_t b;
+    if (readByte(devAddr, regAddr, &b) != 0) {
+        uint8_t mask = (0xFF << (8 - length)) | (0xFF >> (bitStart + length - 1));
+        data <<= (8 - length);
+        data >>= (7 - bitStart);
+        b &= mask;
+        b |= data;
+        return writeByte(devAddr, regAddr, b);
+    } else {
+        return false;
+    }
+}
+
+/** Write multiple bits in a 16-bit device register.
+ * @param devAddr I2C slave device address
+ * @param regAddr Register regAddr to write to
+ * @param bitStart First bit position to write (0-15)
+ * @param length Number of bits to write (not more than 16)
+ * @param data Right-aligned value to write
+ * @return Status of operation (true = success)
+ */
+bool I2Cdev::writeBitsW(uint8_t devAddr, uint8_t regAddr, uint8_t bitStart, uint8_t length, uint16_t data) {
+    //              010 value to write
+    // fedcba9876543210 bit numbers
+    //    xxx           args: bitStart=12, length=3
+    // 01000000         shift left (16 - length)    ]
+    // 00001000         shift right (15 - bitStart) ] --- two shifts ensure all non-important bits are 0
+    // 11100011         mask byte
+    // 10101111         original value (sample)
+    // 10100011         original & mask
+    // 10101011         masked | value
+    uint16_t w;
+    if (readWord(devAddr, regAddr, &w) != 0) {
+        uint16_t mask = (0xFFFF << (16 - length)) | (0xFFFF >> (bitStart + length - 1));
+        data <<= (16 - length);
+        data >>= (15 - bitStart);
+        w &= mask;
+        w |= data;
+        return writeWord(devAddr, regAddr, w);
+    } else {
+        return false;
+    }
+}
+
+/** Write single byte to an 8-bit device register.
+ * @param devAddr I2C slave device address
+ * @param regAddr Register address to write to
+ * @param data New byte value to write
+ * @return Status of operation (true = success)
+ */
+bool I2Cdev::writeByte(uint8_t devAddr, uint8_t regAddr, uint8_t data) {
+    return writeBytes(devAddr, regAddr, 1, &data);
+}
+
+/** Write single word to a 16-bit device register.
+ * @param devAddr I2C slave device address
+ * @param regAddr Register address to write to
+ * @param data New word value to write
+ * @return Status of operation (true = success)
+ */
+bool I2Cdev::writeWord(uint8_t devAddr, uint8_t regAddr, uint16_t data) {
+    return writeWords(devAddr, regAddr, 1, &data);
+}
+
+/** Write multiple bytes to an 8-bit device register.
+ * @param devAddr I2C slave device address
+ * @param regAddr First register address to write to
+ * @param length Number of bytes to write
+ * @param data Buffer to copy new data from
+ * @return Status of operation (true = success)
+ */
+bool I2Cdev::writeBytes(uint8_t devAddr, uint8_t regAddr, uint8_t length, uint8_t* data) {
+    #ifdef I2CDEV_SERIAL_DEBUG
+        Serial.print("I2C (0x");
+        Serial.print(devAddr, HEX);
+        Serial.print(") writing ");
+        Serial.print(length, DEC);
+        Serial.print(" bytes to 0x");
+        Serial.print(regAddr, HEX);
+        Serial.print("...");
+    #endif
+    #if (I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO1_WIRE)
+        uint8_t holder;
+    #endif
+    Wire.beginTransmission(devAddr);
+    for (uint8_t i = 0; i < length; i++) {
+        #if (I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO0_WIRE)
+            Wire.send(regAddr + i);
+            Wire.send(data[i]);
+        #elif (I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO1_WIRE)
+            holder = regAddr + i;
+            Wire.write(&holder, 1);
+            Wire.write(data + i, 1);
+        #endif
+        #ifdef I2CDEV_SERIAL_DEBUG
+            Serial.print(data[i], HEX);
+            Serial.print(" ");
+        #endif
+    }
+    // docs say this returns a byte, but it causes a compiler error
+    //uint8_t status = Wire.endTransmission();
+    Wire.endTransmission();
+    #ifdef I2CDEV_SERIAL_DEBUG
+        Serial.println(". Done.");
+    #endif
+    return true; //status == 0;
+}
+
+/** Write multiple words to a 16-bit device register.
+ * @param devAddr I2C slave device address
+ * @param regAddr First register address to write to
+ * @param length Number of words to write
+ * @param data Buffer to copy new data from
+ * @return Status of operation (true = success)
+ */
+bool I2Cdev::writeWords(uint8_t devAddr, uint8_t regAddr, uint8_t length, uint16_t* data) {
+    #ifdef I2CDEV_SERIAL_DEBUG
+        Serial.print("I2C (0x");
+        Serial.print(devAddr, HEX);
+        Serial.print(") writing ");
+        Serial.print(length, DEC);
+        Serial.print(" words to 0x");
+        Serial.print(regAddr, HEX);
+        Serial.print("...");
+    #endif
+    #if (I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO1_WIRE)
+        uint8_t holder;
+    #endif
+    Wire.beginTransmission(devAddr);
+    for (uint8_t i = 0; i < length * 2; i++) {
+        #if (I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO0_WIRE)
+            Wire.send(regAddr + i);
+            Wire.send((uint8_t)(data[i++] >> 8)); // send MSB
+            Wire.send((uint8_t)data[i]);          // send LSB
+        #elif (I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO1_WIRE)
+            holder = regAddr + i;
+            Wire.write(&holder, 1); // send address
+            holder = (uint8_t)(data[i++] >> 8);
+            Wire.write(&holder, 1); // send MSB
+            holder = (uint8_t)(data[i]);
+            Wire.write(&holder, 1); // send LSB
+        #endif
+        #ifdef I2CDEV_SERIAL_DEBUG
+            Serial.print(data[i], HEX);
+            Serial.print(" ");
+        #endif
+    }
+    // docs say this returns a byte, but it causes a compiler error
+    //uint8_t status = Wire.endTransmission();
+    Wire.endTransmission();
+    #ifdef I2CDEV_SERIAL_DEBUG
+        Serial.println(". Done.");
+    #endif
+    return true; //status == 0;
+}
+
+/** Default timeout value for read operations.
+ * Set this to 0 to disable timeout detection.
+ */
+uint16_t I2Cdev::readTimeout = I2CDEV_DEFAULT_READ_TIMEOUT;
+
+
---
+++ keyglove/keyglove.ino
@@ -0,0 +1,150 @@
+// Keyglove Controller source code - Main setup/loop controller
+// 10/1/2010 by Jeff Rowberg <jeff@rowberg.net>
+
+/* ============================================
+Controller code is placed under the MIT license
+Copyright (c) 2011 Jeff Rowberg
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+===============================================
+*/
+
+// =============================================================================
+// specific build hardware/debug options (EDIT THIS FILE FOR YOUR HARDWARE)
+// =============================================================================
+
+#include "config.h"
+
+#include <Wire.h>
+
+// =============================================================================
+// !!! NO EDITING SHOULD BE NECESSARY BEYOND THIS POINT !!!
+// =============================================================================
+
+/* ===============================================
+ * UNIVERSAL CONTROLLER DECLARATIONS
+=============================================== */
+
+uint16_t counter;           // loops at 1000
+uint32_t ticks;             // ticks every 1000, never loops
+uint32_t t;                 // time/benchmark
+uint32_t i, j;              // global loop indexes for convenience and to avoid dynamic reallocation
+
+bool activeAccelerometer;
+bool activeGyroscope;
+bool activeMagnetometer;
+bool activeGestures;
+bool activeTouch;
+bool activeKeyboard;
+bool activeMouse;
+bool activeJoystick;
+bool activeSerial;
+
+uint16_t opt_touch_detect_threshold = 20;   // number of milliseconds required for a touch to register as real
+uint8_t  opt_motion_sampling_div = 12;      // how many ticks in between each accel/gyro reading
+uint8_t  opt_enable_calibration = 0;        // whether calibration should be enabled
+
+// debug constant definitions
+#include "debug.h"
+
+// hardware constant option definitions
+#include "hardware.h"
+
+// LUFA framework Arduino wrapper
+#ifdef LUFA_ARDUINO_WRAPPER
+    // the Keyglove LUFA code release makefile defines the LUFA_ARDUINO_WRAPPER
+    // constant for all compiled source files, including this one...LUFA is not
+    // automatically Arduino-friendly, so a wrapper is required (like LUFAduino,
+    // but not)
+    #include "ArduinoWrapper.h"
+#endif
+
+// pin assigment constants
+#include "pins.h"
+
+// pre-processor controlled global variable initializations and setup() calls
+#include "setup.h"
+
+/* ===============================================
+ * MAIN SETUP ROUTINE
+=============================================== */
+void setup() {
+    // call main setup function (see setup.h for details)
+    keyglove_setup();
+
+    // initialize benchmark
+    t = millis();
+}
+
+void loop() {
+    // read all defined and active motion sensors
+    #ifdef ENABLE_ACCELEROMETER
+        if (activeAccelerometer && counter % 10 == 0) update_motion_accelerometer();
+    #endif
+    #ifdef ENABLE_GYROSCOPE
+        if (activeGyroscope && counter % 10 == 0) update_motion_gyroscope();
+    #endif
+    #ifdef ENABLE_MAGNETOMETER
+        if (activeMagnetometer && counter % 10 == 0) update_motion_magnetometer();
+    #endif
+    #ifdef ENABLE_FUSION
+        if (activeFusion && counter % 10 == 0) update_motion_fusion();
+    #endif
+
+    // process motion into gestures
+    //if (activeGestures) update_gestures();
+
+    // process touch sensor status
+    if (activeTouch) update_touch();
+
+    // keyboard is almost always done in real time, this is usually not necessary
+    if (activeKeyboard) update_keyboard();
+
+    // send any unsent mouse control data
+    if (activeMouse && counter) update_mouse();
+
+    // send any unsent joystick control data
+    if (activeJoystick) update_joystick();
+
+    // increment loop tick counter
+    counter++;
+
+    // update counter-based feedback (e.g. blinking LEDs)
+    #if (KG_FEEDBACK & KG_FEEDBACK_RGB) > 0
+        update_feedback_rgb();
+    #endif /* KG_FEEDBACK_RGB */
+
+    // test counter and reset if necessary
+    if (counter == 1000) {
+        counter = 0;
+        ticks++;
+        #if (KG_FEEDBACK & KG_FEEDBACK_BLINK) > 0
+            update_feedback_blink();
+        #endif /* KG_FEEDBACK_BLINK */
+
+        DEBUG_PRN_BENCHMARK("benchmark 1000 ");
+        DEBUG_PRN_BENCHMARK(millis() - t);
+        DEBUG_PRN_BENCHMARK(" ");
+        DEBUG_PRNL_BENCHMARK(ticks);
+    }
+
+    t = millis();
+}
+
+
