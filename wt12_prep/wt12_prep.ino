4efcca93a943b4a20259e100cba7fd7378bba8e3
jrowberg <jeff@rowberg.net>
2012-07-04 13:18:54 -0400

    Adding instructive comments to WT12 Prep sketch

--- wt12_prep/wt12_prep.ino
+++ wt12_prep/wt12_prep.ino
@@ -1,3 +1,46 @@
+// Keyglove controller source code - WT12 preparation shortcut sketch
+// 7/4/2012 by Jeff Rowberg <jeff@rowberg.net>
+
+// This is a shortcut sketch for making sure your WT12 is correctly configured
+// for use with the Keyglove. This will ultimately be rolled into the main
+// Keyglove firmware, but for the moment there is a separate sketch to make it
+// easier to do.
+
+/* ============================================
+Controller code is placed under the MIT license
+Copyright (c) 2012 Jeff Rowberg
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+===============================================
+*/
+
+
+
+// NOTE: Under normal cirumstances, the host MCU must be running at a minimum of
+// 16MHz for reliable serial communication at 115200 baud. The error rate is too
+// high at 8MHz or lower. If you're using a Teensy board, make sure the CPU is
+// set to 16MHz while running this sketch even if the device has been modified
+// for 3.3v operation. 16MHz @ 3.3v is overclocking, but should be safe at least
+// for short periods of time.
+
+
+
 // use the following line if using Teensy or Teensy++
 // if using the Arduino Leonardo or Mega, comment it out
 HardwareSerial Serial1 = HardwareSerial();
