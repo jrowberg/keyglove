2a828a05bd06745d6a8d6fb6310d9a9160fe3ad2
Jeff Rowberg <jeff.rowberg@bluegiga.com>
2014-12-13 20:24:21 -0500

    Change hardware configuration definitions to hex values instead of decimals for clarity

--- controller/arduino/keyglove/hardware.h
+++ controller/arduino/keyglove/hardware.h
@@ -45,82 +45,85 @@ THE SOFTWARE.
 
 /* Main controller board options. Only one board may be selected at compile time. (defined in KG_BOARD) */
 
-#define KG_BOARD_USER                   0           ///< Totally custom board, you better know what you're doing
-#define KG_BOARD_TEENSYPP2_T37          100         ///< AT90USB128x MCU, 46 I/O pins and prototype/kit design, 37 touch sensors
-#define KG_BOARD_TEENSYPP2_T19          101         ///< AT90USB128x MCU, 46 I/O pins and prototype/kit design, 19 touch sensors
-#define KG_BOARD_ARDUINO_DUE            200         ///< **NOT SUPPORTED YET:** SAM3X8E MCU, 50+ I/O pins and prototype/Arduino design
-#define KG_BOARD_KEYGLOVE100            300         ///< **NOT SUPPORTED YET:** SAM3X8E MCU, 50+ I/O pins and custom design
+#define KG_BOARD_USER                   0x00        ///< Totally custom board, you better know what you're doing
+#define KG_BOARD_TEENSYPP2_T37          0x10        ///< AT90USB128x MCU, 46 I/O pins and prototype/kit design, 37 touch sensors
+#define KG_BOARD_TEENSYPP2_T19          0x11        ///< AT90USB128x MCU, 46 I/O pins and prototype/kit design, 19 touch sensors
+#define KG_BOARD_ARDUINO_DUE            0x20        ///< **NOT SUPPORTED YET:** SAM3X8E MCU, 50+ I/O pins and prototype/Arduino design
+#define KG_BOARD_KEYGLOVE100            0x30        ///< **NOT SUPPORTED YET:** SAM3X8E MCU, 50+ I/O pins and custom design
 
 
 
 /* Flex sensor options. Only one choice may be selected at the same time. (defined in KG_FLEX) */
 
-#define KG_FLEX_NONE                    0           ///< No flex sensors used
-#define KG_FLEX_5FINGERS                1           ///< **NOT SUPPORTED YET:** Full 5-finger complement
+#define KG_FLEX_NONE                    0x00        ///< No flex sensors used
+#define KG_FLEX_5FINGERS                0x01        ///< **NOT SUPPORTED YET:** Full 5-finger complement
 
 
 
 /* Pressure sensor options. Only one choice may be selected at the same time. (defined in KG_PRESSURE) */
 
-#define KG_PRESSURE_NONE                0           ///< No pressure sensors used
-#define KG_PRESSURE_5TIPS               1           ///< **NOT SUPPORTED YET:** Full 5-fingertip complement
+#define KG_PRESSURE_NONE                0x00        ///< No pressure sensors used
+#define KG_PRESSURE_5TIPS               0x01        ///< **NOT SUPPORTED YET:** Full 5-fingertip complement
 
 
 
 /* Hand selection options. Only one choice may be selected at the same time. (defined in KG_HAND) */
 
-#define KG_HAND_RIGHT                   1           ///< Right-hand sensor connection orientation (default)
-#define KG_HAND_LEFT                    2           ///< Left-hand sensor connection orientation (relevant for kit, others automatic)
+#define KG_HAND_RIGHT                   0x01        ///< Right-hand sensor connection orientation (default)
+#define KG_HAND_LEFT                    0x02        ///< Left-hand sensor connection orientation (relevant for kit, others automatic)
 
 
 
 /* Dual glove communication options. Only one choice may be selected at the same time. (defined in KG_DUALGLOVE) */
 
-#define KG_DUALGLOVE_NONE               0           ///< No dual-glove support
-#define KG_DUALGLOVE_RFM22B             1           ///< **NOT SUPPORTED YET:** Wireless connection with RFM22B
+#define KG_DUALGLOVE_NONE               0x00        ///< No dual-glove support
+#define KG_DUALGLOVE_RFM22B             0x01        ///< **NOT SUPPORTED YET:** Wireless connection with RFM22B
 
 
 
 /* Host interface options. Multiple interfaces may be enabled at the same time. (defined in KG_HOSTIF) */
 
-#define KG_HOSTIF_NONE                  0           ///< Don't communicate (weird, maybe you have a reason)
-#define KG_HOSTIF_USB_SERIAL            1           ///< Hardware USB serial (requires ARM, AT90USB* or ATMega32U* MCU)
-#define KG_HOSTIF_USB_RAWHID            2           ///< Hardware USB raw HID (requires ARM, AT90USB* or ATMega32U* MCU)
-#define KG_HOSTIF_USB_HID               4           ///< Hardware USB HID (requires ARM, AT90USB* or ATMega32U* MCU)
-#define KG_HOSTIF_BT2_SERIAL            8           ///< Bluetooth v2 serial (requires Bluegiga WT12 w/iWRAP)
-#define KG_HOSTIF_BT2_RAWHID            16          ///< Bluetooth v2 raw HID (requires Bluegiga WT12 w/iWRAP v5+)
-#define KG_HOSTIF_BT2_HID               32          ///< Bluetooth v2 HID (requires Bluegiga WT12 w/iWRAP)
+#define KG_HOSTIF_NONE                  0x00        ///< Don't communicate (weird, maybe you have a reason)
+#define KG_HOSTIF_USB_SERIAL            0x01        ///< Hardware USB serial (requires ARM, AT90USB* or ATMega32U* MCU)
+#define KG_HOSTIF_USB_RAWHID            0x02        ///< Hardware USB raw HID (requires ARM, AT90USB* or ATMega32U* MCU)
+#define KG_HOSTIF_USB_HID               0x04        ///< Hardware USB HID (requires ARM, AT90USB* or ATMega32U* MCU)
+#define KG_HOSTIF_BT2_SERIAL            0x08        ///< Bluetooth v2 serial (requires Bluegiga WT12 w/iWRAP)
+#define KG_HOSTIF_BT2_RAWHID            0x10        ///< Bluetooth v2 raw HID (requires Bluegiga WT12 w/iWRAP v5+)
+#define KG_HOSTIF_BT2_HID               0x20        ///< Bluetooth v2 HID (requires Bluegiga WT12 w/iWRAP)
 
 
 
 /* HID support options. Multiple HID types may be enabled at the same time. (defined in KG_HID) */
 
-#define KG_HID_KEYBOARD                 1           ///< Include standard HID keyboard support routines
-#define KG_HID_MOUSE                    2           ///< Include standard HID mouse support routines
+#define KG_HID_NONE                     0x00        ///< No standard HID support
+#define KG_HID_KEYBOARD                 0x01        ///< Include standard HID keyboard support routines
+#define KG_HID_MOUSE                    0x02        ///< Include standard HID mouse support routines
 
 
 
 /* Motion sensor options. Multiple sensors may be enabled, provided they do not compete. (defined in KG_MOTION) */
 
-#define KG_MOTION_MPU6050_HAND          1           ///< 6-axis I2C digital accel/gyro on back of hand
-#define KG_MOTION_MPU6050_INDEXTIP      2           ///< 6-axis I2C digital accel/gyro on index fingertip
+#define KG_MOTION_NONE                  0x00        ///< No motion support
+#define KG_MOTION_MPU6050_HAND          0x01        ///< 6-axis I2C digital accel/gyro on back of hand
+#define KG_MOTION_MPU6050_INDEXTIP      0x02        ///< 6-axis I2C digital accel/gyro on index fingertip
 
 
 
 /* Sensory feedback. Multiple options may be enabled. (defined in KG_FEEDBACK) */
 
-#define KG_FEEDBACK_BLINK               1           ///< Single LED (e.g. on Arduino/Teensy boards)
-#define KG_FEEDBACK_PIEZO               2           ///< Piezo buzzer for sound
-#define KG_FEEDBACK_VIBRATE             4           ///< Vibration motor for haptic feedback
-#define KG_FEEDBACK_RGB                 8           ///< Combined RGB LED for visual feedback
+#define KG_FEEDBACK_NONE                0x00        ///< No feedback support
+#define KG_FEEDBACK_BLINK               0x01        ///< Single LED (e.g. on Arduino/Teensy boards)
+#define KG_FEEDBACK_PIEZO               0x02        ///< Piezo buzzer for sound
+#define KG_FEEDBACK_VIBRATE             0x04        ///< Vibration motor for haptic feedback
+#define KG_FEEDBACK_RGB                 0x08        ///< Combined RGB LED for visual feedback
 
 
 
 /* Sensory feedback connection. Only one choice may be selected at a time. (defined in KG_FEEDBACKCONN) */
 
-#define KG_FEEDBACKCONN_NONE            0           ///< Don't do anything with feedback
-#define KG_FEEDBACKCONN_DIRECT          1           ///< Direct connection to I/O pins
-#define KG_FEEDBACKCONN_I2C             2           ///< Custom I2C-based RGB/vibe/piezo module
+#define KG_FEEDBACKCONN_NONE            0x00        ///< Don't do anything with feedback
+#define KG_FEEDBACKCONN_DIRECT          0x01        ///< Direct connection to I/O pins
+#define KG_FEEDBACKCONN_I2C             0x02        ///< Custom I2C-based RGB/vibe/piezo module
 
 
 
